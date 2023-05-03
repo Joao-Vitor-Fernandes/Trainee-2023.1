6e307b4c55c1b7d77eae786d36071bbc49173eeb
Victoria <victoria.yamashita@estudante.ufjf.br>
2023-05-03 15:11:14 -0300

    add modal delete posts

--- Phonefy/public/js/modal.js
+++ Phonefy/public/js/modal.js
@@ -1,25 +1,32 @@
 var modal_edit = document.getElementById("myModal");
-var modal_delete = document.getElementById("myModal");
+var modal_delete = document.getElementById("myModal-delete");
 
 var btn_edit = document.getElementById("myBtn-edit");
 var btn_delete = document.getElementById("myBtn-delete");
 
 var span = document.getElementsByClassName("close")[0];
+var span_delete = document.getElementsByClassName("close-delete")[0];
+
 
 btn_edit.onclick = function() {
   modal_edit.style.display = "block";
+}
+btn_delete.onclick = function() {
+    modal_delete.style.display = "block";
 }
 
 span.onclick = function() {
   modal_edit.style.display = "none";
 }
+span_delete.onclick = function() {
+    modal_delete.style.display = "none";
+  }
 
 window.onclick = function(event) {
   if (event.target == modal_edit) {
     modal_edit.style.display = "none";
   }
-}
-
-btn_delete.onclick = function() {
-    modal_delete.style.display = "block";
+  if (event.target == modal_delete) {
+    modal_delete.style.display = "none";
+  }
 }
