93a6a86ba9c1c60fd0af52a1528a2b622a679181
Victoria <victoria.yamashita@estudante.ufjf.br>
2023-05-03 17:50:13 -0300

    css default button

--- Phonefy/public/js/modal.js
+++ Phonefy/public/js/modal.js
@@ -1,11 +1,14 @@
 var modal_edit = document.getElementById("myModal");
 var modal_delete = document.getElementById("myModal-delete");
+var modal_create = document.getElementById("myModal-create");
 
 var btn_edit = document.getElementById("myBtn-edit");
 var btn_delete = document.getElementById("myBtn-delete");
+var btn_create = document.getElementById("myBtn-create");
 
 var span = document.getElementsByClassName("close")[0];
 var span_delete = document.getElementsByClassName("close-delete")[0];
+var span_create = document.getElementsByClassName("close-create")[0];
 
 
 btn_edit.onclick = function() {
@@ -13,6 +16,9 @@ btn_edit.onclick = function() {
 }
 btn_delete.onclick = function() {
     modal_delete.style.display = "block";
+}
+btn_create.onclick = function() {
+    modal_create.style.display = "block";
 }
 
 span.onclick = function() {
