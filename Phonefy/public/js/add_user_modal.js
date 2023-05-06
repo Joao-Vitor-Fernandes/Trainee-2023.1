115751d445360a3d4ee3ab97470810ccdaa8a18f
luiz_oliveiraa <luiz.pereira@ice.ufjf.br>
2023-05-06 17:38:40 -0300

    integrando modal com a tabela

--- Phonefy/public/js/add_user_modal.js
+++ Phonefy/public/js/add_user_modal.js
@@ -1,6 +1,6 @@
 var modal = document.getElementById("myModal");
 var btn = document.getElementById("myBtn");
-var span = document.getElementsByClassName("close")[0];
+var span = document.getElementsByClassName("close-l")[0];
 
 btn.onclick = function() {
   modal.style.display = "block";
