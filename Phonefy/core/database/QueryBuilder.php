0c5321d1b8da7521c25c31ba372cbd7b2aee7b4e
Rayane de Oliveira <rayanecontato1@gmail.com>
2023-05-31 17:42:05 -0300

    query builder do delete

--- Phonefy/core/database/QueryBuilder.php
+++ Phonefy/core/database/QueryBuilder.php
@@ -44,4 +44,21 @@ class QueryBuilder
             die($e->getMessage());
         }
     }
+
+    public function delete($table, $id){
+        $sql = sprintf(
+            'DELETE FROM %s WHERE %s',
+            $table,
+            "id = :id"
+        );
+
+        try {
+            $statement = $this->pdo->prepare($sql);
+
+            $statement->execute(compact('id'));
+        } catch (Exception $e){
+            die("Ocorreu um erro ao tentar excluir do banco de dados: {$e->getMessage()}");
+        }
+    }
 }
+
