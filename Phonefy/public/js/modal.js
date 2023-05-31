32af0b8d25217916730e906d8a35e7a1d53594d1
luiz_oliveiraa <luiz.pereira@ice.ufjf.br>
2023-05-31 03:49:18 -0300

    modificando modal.js para alterar url

--- Phonefy/public/js/modal.js
+++ Phonefy/public/js/modal.js
@@ -7,7 +7,7 @@ function load(url, element)
     element.innerHTML += req.responseText;
 }
 
-function open_modal(b, m, s, i)
+function open_modal(b, m, s, i, route)
 {
     btn = document.getElementById(b);
     modal = document.getElementById(m);
@@ -16,20 +16,25 @@ function open_modal(b, m, s, i)
     modal.style.display = "block";
     console.log('clicou');
 
+    var previousURL = window.location.href; // Armazena a URL atual
+    history.pushState({}, '', route); //Modifica a url
+
     span.onclick = function() {
         modal.style.display = "none";
         console.log('saiu');
         console.log(span);
+        history.replaceState({}, '', previousURL); // Restaura a URL anterior
     }
 
     window.onclick = function(event) {
         if (event.target == modal) {
             modal.style.display = "none";
+            history.replaceState({}, '', previousURL); // Restaura a URL anterior
         }
     }
 }
 
 load('../../../app/views/admin/view_modal.html', document.getElementsByClassName('load_modal')[0]);
-load('../../../app/views/admin/add_modal.html', document.getElementsByClassName('load_modal')[0]);
+load('../../../app/views/admin/add_modal.view.php', document.getElementsByClassName('load_modal')[0]);
 load('../../../app/views/admin/edit_modal.html', document.getElementsByClassName('load_modal')[0]);
 load('../../../app/views/admin/delete_modal.html', document.getElementsByClassName('load_modal')[0]);
