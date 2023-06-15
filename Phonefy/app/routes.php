e0a270a4fa0c54765d3813c63380b10d0cae82ce
Victoria <victoria.yamashita@estudante.ufjf.br>
2023-06-14 23:17:06 -0300

    renomeia rotas

--- Phonefy/app/routes.php
+++ Phonefy/app/routes.php
@@ -6,13 +6,10 @@ use App\Core\Router;
 
 $router->get('index', 'ExampleController@index');
 
-$router->get('lista_de_usuarios', 'UserController@lista_de_usuarios');
-
-$router->get('show', 'UserController@show');
-
-$router->post('create', 'UserController@create');
-$router->post('delete', 'UserController@delete');
-$router->post('update', 'UserController@update');
+$router->get('admin/usuarios', 'UserController@view_usuarios');
+$router->post('admin/usuarios/create', 'UserController@create_usuarios');
+$router->post('admin/usuarios/delete', 'UserController@delete_usuarios');
+$router->post('admin/usuarios/update', 'UserController@update_usuarios');
 
 
 ?> 
