5e7b07f05faae66a30d893d6c039fe56038067e7
Victoria <victoria.yamashita@estudante.ufjf.br>
2023-06-17 18:04:10 -0300

    adjusts

--- Phonefy/app/Controllers/SiteController.php
+++ Phonefy/app/Controllers/SiteController.php
@@ -21,7 +21,7 @@ class SiteController
             }
         }
 
-        $items_per_page = 3;
+        $items_per_page = 6;
         $start_limit = $items_per_page * $page - $items_per_page;
         $rows_count = App::get('database')->countAll('posts');
         
@@ -31,9 +31,25 @@ class SiteController
         
         $total_pages = ceil($rows_count / $items_per_page);
         $posts = App::get('database')->selectAll('posts', $start_limit, $items_per_page);
+        $users = App::get('database')->selectAll('users');
+        // Associar o nome do autor aos posts(autor é o id do user)
+        foreach ($posts as $post) {
+            $author = $this->getUserById($users, $post->author);
+            $post->author_name = $author->name;
+        }
 
         return view('site/lista_de_posts', compact("posts", "page", "total_pages"));
 
         // return view('site/lista_de_posts');
+    }
+    private function getUserById($users, $userId)
+    {
+        foreach ($users as $user) {
+            if ($user->id === $userId) {
+                return $user;
+            }
+        }
+
+        return null; // Caso o usuário não seja encontrado
     }
 }
