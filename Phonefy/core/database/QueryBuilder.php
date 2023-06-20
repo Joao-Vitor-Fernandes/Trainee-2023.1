4ad02b7f900bd5bd8cfc67252c0529ebf663b2a7
Victoria <victoria.yamashita@estudante.ufjf.br>
2023-06-20 18:53:02 -0300

    adjust pdo in login/autenticar

--- Phonefy/core/database/QueryBuilder.php
+++ Phonefy/core/database/QueryBuilder.php
@@ -173,14 +173,20 @@ class QueryBuilder
         // $sql = "select id from {$table} where email = '{$email}' and password = '{$senha}'";
         $sql = sprintf('select id from %s where %s and %s',
             $table,
-            "email = '$email'",
-            "password = '$senha'"
+            "email = :email",
+            "password = :senha"
         );
-
+        
         // die(var_dump($sql));
         try {
             $statement = $this->pdo->prepare($sql);
-            $statement->execute();
+            $statement->execute(
+                [
+                    'email' => $email,
+                    'senha' => $senha
+                ]
+            );
+            
             // return $statement->fecthOne(PDO::FETCH_ASSOC);
             // die(var_dump(intval($statement->fetch(PDO::FETCH_NUM)[0])));
             return intval($statement->fetch(PDO::FETCH_NUM)[0]);
