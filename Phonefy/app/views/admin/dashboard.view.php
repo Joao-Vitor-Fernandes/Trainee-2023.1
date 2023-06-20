e3cf0a830b1c227d42a955dcb522b029ab21078d
Victoria <victoria.yamashita@estudante.ufjf.br>
2023-06-20 01:09:54 -0300

    replicando a sidebar

--- Phonefy/app/views/admin/dashboard.view.php
+++ Phonefy/app/views/admin/dashboard.view.php
@@ -5,13 +5,16 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Dashboard</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
         <link rel="stylesheet" href="../../../public/css/dashboard.css">
+
+        <link rel="stylesheet" href="../../../public/css/sidebar.css"/>
     </head>
 
     <body>
-    <?php require './app/views/admin/sidebar.html' ?>
         <div class="dashboard">
-            <button class="sidebar-icon"><ion-icon name="chevron-forward-outline"></ion-icon></button>
+            <?php require './app/views/admin/sidebar.html' ?>
+            <!-- <button class="sidebar-icon"><ion-icon name="chevron-forward-outline"></ion-icon></button> -->
 
             <div class="dashboard-nav">
                 <div class="icon-name">
@@ -76,6 +79,8 @@
 
         <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
         <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 
     </body>
 </html>
