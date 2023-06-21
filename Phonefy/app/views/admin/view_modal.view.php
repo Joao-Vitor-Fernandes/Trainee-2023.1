36f1540faf75b2b32b31cf20594d46b80f6c0253
luiz_oliveiraa <luiz.pereira@ice.ufjf.br>
2023-06-21 14:20:29 -0300

    Corrigindo acentos no conteudo

--- Phonefy/app/views/admin/view_modal.view.php
+++ Phonefy/app/views/admin/view_modal.view.php
@@ -41,7 +41,7 @@
                 </div>
         
                 <div class="caixa-conteudo">
-                    <label for="conteudo">Conteudo:</label>
+                    <label for="conteudo">Conteúdo:</label>
                     <textarea rows="6" id="conteudo" name="conteudo" readonly disabled>O aguardado nono álbum do The National acaba de ser lançado e, boa notícia para os fãs, mostra o quinteto em ótima forma. "Firts Two Pages Of Frankenstein" não chega com a banda tentando se reinventar. A música continua melancólica, bela e climática, com a voz de barítiono de Matt Berninger sempre como um destaque. </textarea>
                 </div>
     
