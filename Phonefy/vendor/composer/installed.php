3ebeb6bb763cd6ff71a649ad1149ebeb716197f1
Victoria <victoria.yamashita@estudante.ufjf.br>
2023-06-20 23:45:06 -0300

    adjusts in login - navbar/footer and centralize

--- Phonefy/vendor/composer/installed.php
+++ Phonefy/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '2a48b459202313333fcc759a283405b3e91bf8bd',
+        'reference' => 'b25fc5565e0fa7bae82dc5a1d9e1702ac0916a76',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '2a48b459202313333fcc759a283405b3e91bf8bd',
+            'reference' => 'b25fc5565e0fa7bae82dc5a1d9e1702ac0916a76',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
